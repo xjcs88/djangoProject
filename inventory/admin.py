8be8771789017faed93edee8fd48bb42e15cbaf1
Simon Chen <xjcs88@gmail.com>
2022-04-28 22:39:38 +1000

    Use code provided by codecademy for draft. Add pagenator for purchases view; changed the language code to en-au.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import RecipeRequirement, MenuItem, Ingredient, Purchase
 
 # Register your models here.
+[admin.site.register(X) for X in [RecipeRequirement, MenuItem, Ingredient, Purchase]]
